310d502c03c1ee3f3a4bcdd07bda26947467e824
Norahfalharbi <s202249220@kfupm.edu.sa>
2025-10-24 17:34:43 +0300

    initial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// Get the current date and time
+const now = new Date();
+let greeting;
 
+// Check the current hour and decide which greeting to use
+if (now.getHours() < 12) {
+  greeting = "Good Morning";
+} else if (now.getHours() < 18) {
+  greeting = "Good Afternoon";
+} else {
+  greeting = "Good Evening";
+}
+
+// Show a popup alert with the greeting + current time
+alert(
+  greeting + " Welcome to My Website" + 
+  " and Time is " + now.getHours() + ":" + now.getMinutes()
+);
